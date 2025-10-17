89ebae2ae887b7f2b2249394a301f8c2e27b26b9
Jaime Jeovanny Cortez Flores <jaimecortez@ufg.edu.sv>
2025-10-16 19:59:51 -0600

    Creacion de proyecto laravel

---
+++ cortezflores_20251016/routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ZonaController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+Route::get('/zonas',[ZonaController::class,'obtenerZonas']); //PLURAL
+
+Route::get('/zona/{idzona}',[ZonaController::class,'obtenerZona']); //SINGULAR
