c109327bbd4f3952f02a2c2071427338e84508b8
Jaime Jeovanny Cortez Flores <jaimecortez@ufg.edu.sv>
2025-10-21 20:10:41 -0600

    Correccion de endpoints

--- cortezflores_20251016/app/Http/Controllers/ZonaController.php
+++ cortezflores_20251016/app/Http/Controllers/ZonaController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Zona;
 
 class ZonaController extends Controller
 {
@@ -11,33 +12,110 @@ class ZonaController extends Controller
     public function obtenerZonas(){
 
         $Zona = new Zona();
+
+        $satisfactorio = false;
+        $estado = 0;
+        $mensaje = "";
+        $errores = [];
+        $valores = [];
+
         $valores = $Zona::all();
+
+        //VERIFICACION DE EXISTENCIA DE DATOS
+        if(!empty($valores)){
+            //SI SE ENCONTRARON DATOS
+            $satisfactorio = true;
+            $estado = 200;
+            $mensaje = "Valores encontrados";
+            $errores = [
+                "code" => 200,
+                "msg" => ""
+            ];
+        }else{
+            //NO SE ENCONTRARON DATOS
+            $satisfactorio = false;
+            $estado = 404;
+            $mensaje = "No se han encontrado valores";
+            $errores = [
+                "code" => 404,
+                "msg" => "Datos no encontrados"
+            ];
+        }
+
+        //SE CREA LA VARIABLE DE SALIDA
         $respuesta = [
-            "success" => true,
-            "msg" => "Valores devueltos por el EndPoint",
+            "success" => $satisfactorio,
+            "status" => $estado,
+            "msg" => $mensaje,
             "data" => $valores,
-            "error" => "",
+            "errors" => $errores,
             "total" => sizeof($valores)
         ];
 
-        return response()->json($respuesta,200);
+        //SE RETORNA EL MENSAJE AL USUARIO
+        return response()->json($respuesta,$estado);
 
     }
 
 
-    public function obtenerZona($idzona){
+    public function obtenerZona(int $idzona = 0){
 
-        $Zona = new Zona();
-        $valores = $Zona->where('id_zona',$idzona)->get();
+        $satisfactorio = false;
+        $estado = 0;
+        $mensaje = "";
+        $errores = [];
+        $valores = [];
+
+        if($idzona > 0){
+            //EL PARAMETRO DE $idzona ES MAYOR QUE CERO
+            $Zona = new Zona();
+            $valores = $Zona->where('id_zona',$idzona)->get();
+
+            //VERIFICACION DE EXISTENCIA DE DATOS
+            if(!empty($valores)){
+                //SI SE ENCONTRARON DATOS
+                $satisfactorio = true;
+                $estado = 200;
+                $mensaje = "Valores encontrados";
+                $errores = [
+                    "code" => 200,
+                    "msg" => ""
+                ];
+            }else{
+                //NO SE ENCONTRARON DATOS
+                $satisfactorio = false;
+                $estado = 404;
+                $mensaje = "No se han encontrado valores";
+                $errores = [
+                    "code" => 404,
+                    "msg" => "Datos no encontrados"
+                ];
+            }//fin del if(!empty($valores)){
+
+        }else{
+            //NO SE HA ENVIADO UN VALOR PARA EL PARAMETRO $idzona
+            $satisfactorio = false;
+            $estado = 400;
+            $mensaje = "No se ha enviado el parametro obligatorio";
+            $errores = [
+                "code" => 400,
+                "msg" => "El identificador de la zona esta vacio"
+            ];
+
+        }//fin del if($idzona > 0){
+        
+        //SE CREA LA VARIABLE DE SALIDA
         $respuesta = [
-            "success" => true,
-            "msg" => "Valores devueltos por el EndPoint",
+            "success" => $satisfactorio,
+            "status" => $estado,
+            "msg" => $mensaje,
             "data" => $valores,
-            "error" => "",
+            "errors" => $errores,
             "total" => sizeof($valores)
         ];
 
-        return response()->json($respuesta,200);
+        //SE MUESTRA EL MENSAJE AL USUARIO
+        return response()->json($respuesta,$estado);
     }
 
 
